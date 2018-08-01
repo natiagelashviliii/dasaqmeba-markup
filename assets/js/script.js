d9de32968ad67a5a8f5dceb62d7a1b4f14f1a36f
natiagelashviliii <39669499+natiagelashviliii@users.noreply.github.com>
2018-08-01 19:23:23 +0400

    add responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
+	
 	$('select').formSelect();
 	$('.tabs').tabs({});
 	$('.datepicker').datepicker();
+	$('.modal').modal();
 
 	$('.login-registration-tabs li a').on('click', function(){
 		let id 		= $(this).attr('id');
