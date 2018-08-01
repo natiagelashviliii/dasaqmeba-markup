4dec04851dcd84c84d011468357abf95c482e0b3
natiagelashviliii <natiagelashvili@geolab.edu.ge>
2018-08-02 00:02:41 +0400

    add index navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,34 @@ $(document).ready(function(){
 	$('.datepicker').datepicker();
 	$('.modal').modal();
 
+	//bg change on login-registration page
 	$('.login-registration-tabs li a').on('click', function(){
 		let id 		= $(this).attr('id');
 		let imgSrc 	= (id == 'Login') ? 'assets/img/bag.svg' : 'assets/img/binoculars.svg' ;
 		let imgID 	= (id == 'Login') ? 'img-bag' : 'img-binoculars' ;
 		$('.login-reg-img').attr('src', imgSrc);
 		$('.login-reg-img').attr('id', imgID);
+	})
+
+	//rules modal - btn disable enable on scroll
+	$('.rules-modal-content .rules').on('scroll', function() {
+        if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+            $('#rules-agree-btn').attr('disabled', false);
+        } else{
+        	$('#rules-agree-btn').attr('disabled', true);
+        }
+    });
+
+	//aside menu animation
+	$('.aside-menu-btn').on('click', function(e) {
+		e.preventDefault();
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+			$('main').removeClass('main-inside');
+		} else {
+			$(this).addClass('active');
+			$('main').addClass('main-inside');
+		}
 	})
 
 });
